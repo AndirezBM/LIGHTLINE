bafea846d3b512b557fa987a557d5bb6daea892b
AndirezBM <andirezbm@gmail.com>
2025-04-13 01:38:30 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+mport React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <h1>Welcome to LIGHTLINE</h1>
+        <p>This is your sacred space. More features coming soon.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
